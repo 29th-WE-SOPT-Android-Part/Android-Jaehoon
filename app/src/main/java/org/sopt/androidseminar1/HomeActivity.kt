47f491d4be3c5e4a946ef01a62412ef1fcebe4e2
Jaehun-Jo <chojh922@gmail.com>
2021-10-05 08:57:46 +0900

    프로젝트 추가

---
+++ app/src/main/java/org/sopt/androidseminar1/HomeActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.androidseminar1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
