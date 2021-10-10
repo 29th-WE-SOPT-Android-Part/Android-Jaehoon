bf0b301a9a0ca7361d816338c2754b75bfc19f0e
Jaehun-Jo <chojh922@gmail.com>
2021-10-10 20:50:51 +0900

    필수 과제 제출

--- app/src/main/java/org/sopt/androidseminar1/SignInActivity.kt
+++ app/src/main/java/org/sopt/androidseminar1/SignInActivity.kt
@@ -3,19 +3,30 @@ package org.sopt.androidseminar1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.androidseminar1.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySignInBinding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == RESULT_OK) {
+                binding.etId.setText(it.data?.getStringExtra("id"))
+                binding.etPw.setText(it.data?.getStringExtra("pw"))
+            }
+        }
+
         binding.btnLogin.setOnClickListener {
             if(!binding.etId.text.toString().isEmpty() && !binding.etPw.text.toString().isEmpty()) {
+
                 Toast.makeText(this, "${binding.etId.text.toString()}님 환영합니다", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
@@ -26,7 +37,7 @@ class SignInActivity : AppCompatActivity() {
 
         binding.btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            activityResultLauncher.launch(intent)
         }
     }
 }
