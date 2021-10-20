0c4dd6d06c1758e446f21c83e052a0b095548690
Jaehun-Jo <chojh922@gmail.com>
2021-10-21 00:11:04 +0900

    1주차 도전, 심화 과제 완료

--- app/src/main/java/org/sopt/androidseminar1/HomeActivity.kt
+++ app/src/main/java/org/sopt/androidseminar1/HomeActivity.kt
@@ -1,11 +1,30 @@
 package org.sopt.androidseminar1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.sopt.androidseminar1.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        initBtn()
+
+        binding.user = User("조재훈", "26", "INFJ",
+            "안드파트 최고!\n\n안드파트 최고!\n\n안드파트 최고!\n\n안드파트 최고!\n\n안드파트 최고!")
+    }
+
+    private fun initBtn() {
+        binding.btnGit.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/jaehoon-jo"))
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/org/sopt/androidseminar1/SignUpActivity.kt
+++ app/src/main/java/org/sopt/androidseminar1/SignUpActivity.kt
@@ -3,6 +3,7 @@ package org.sopt.androidseminar1
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import org.sopt.androidseminar1.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -11,19 +12,22 @@ class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
 
-        binding.apply {
-            btnSignUp.setOnClickListener {
-                if(!etName.text.toString().isEmpty() && !etId.text.toString().isEmpty() && !etPw.text.toString().isEmpty()) {
-                    intent.putExtra("id", etId.text.toString())
-                    intent.putExtra("pw", etPw.text.toString())
-                    setResult(RESULT_OK, intent)
-                    finish()
-                } else {
-                    Toast.makeText(this@SignUpActivity, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
-                }
+        initBtn()
+    }
+
+    private fun initBtn() {
+        binding.btnSignUp.setOnClickListener {
+            if (!binding.etName.text.isEmpty() && !binding.etId.text.isEmpty() && !binding.etPw.text.isEmpty()
+            ) {
+                intent
+                    .putExtra("id", binding.etId.text)
+                    .putExtra("pw", binding.etPw.text)
+                setResult(RESULT_OK, intent)
+                finish()
+            } else {
+                Toast.makeText(this@SignUpActivity, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
             }
         }
     }
