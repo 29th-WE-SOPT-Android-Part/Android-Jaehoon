20b6f8528983d6364d8f5d2971f9a55590424c6a
Jaehun-Jo <chojh922@gmail.com>
2021-10-21 03:10:45 +0900

    1주차 피드백 수정

--- app/src/main/java/org/sopt/androidseminar1/home/HomeActivity.kt
+++ app/src/main/java/org/sopt/androidseminar1/home/HomeActivity.kt
@@ -1,10 +1,12 @@
-package org.sopt.androidseminar1
+package org.sopt.androidseminar1.home
 
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import org.sopt.androidseminar1.R
+import org.sopt.androidseminar1.User
 import org.sopt.androidseminar1.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
